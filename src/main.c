9674d5e841492933188bdaaaf59d0c8289cf65a5
Albert Shefner <dev_n0emo@tuta.io>
2024-11-12 21:40:59 +0300

    Implement getting port from env

--- src/main.c
+++ src/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "http.h"
 #include "web.h"
 
 int main() {
-    // TODO: get port from env
-    if (!start_server(9000, &handle_request)) {
+    int port = 9000;
+    const char *port_env = getenv("SERVER_PORT");
+
+    if (port_env != NULL) {
+        int new_port = atoi(port_env);
+        if (new_port != 0) {
+            port = new_port;
+        }
+    }
+
+    if (!start_server(port, &handle_request)) {
         // TODO: better logging system
         fprintf(stderr, "Error starting server");
         return 1;
